403f1d7d3804da2c565807006c78e707c20fcd9f
mission-control <mission-control@mission-control-7754f5485-dbsx2>
2017-12-20 15:02:36 +0000

    File changed by admin

--- mc1.groovy
+++ mc1.groovy
@@ -1,6 +1,7 @@
 artifactory('Artifactory1') {
 
   localRepository('docker-local') {
+   description "Made by script from git to MC "
   includesPattern "**/*"
   excludesPattern "" 
   repoLayoutRef "simple-default"
