f3b16321f68eba36965b5d7cf07cefa518c5442f
mission-control <mission-control@mission-control-7754f5485-dbsx2>
2017-12-20 14:58:08 +0000

    File changed by admin

--- mc1.groovy
+++ mc1.groovy
@@ -1,13 +1,12 @@
-repoName = userInput (
-    type : "STRING"
-    description : "Please provide a repository name (local already included)"
-  )
- 
-localRepository(repoName) {
+artifactory('Artifactory1') {
+
+  localRepository('docker-local') {
+  includesPattern "**/*"
+  excludesPattern "" 
+  repoLayoutRef "docker-default"
+  archiveBrowsingEnabled false
+  blackedOut false 
   packageType "docker"
-  description "local docker releases repository"
-  includesPattern "*/**"
-  handleSnapshots true
-  handleReleases true
-  checksumPolicyType "server-generated-checksums"
+  }
+
 }
